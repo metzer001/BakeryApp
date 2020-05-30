6ed7eb14df71d69d66227f4f1a3683f5fccce56a
vaghelad <vaghelad@5PSJD22-LPT>
2020-05-30 21:16:01 +0100

    Added EF Core and Context class

---
+++ Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using BakeryApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BakeryApp.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.ImageName).HasColumnName("ImageFileName");
+        }
+    }
+}
